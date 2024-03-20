9e73ce6fa2f6f1a08ce5615bd94177d518995c06
lrnholanda <lrnholanda@gmail.com>
2024-03-20 14:30:41 -0300

    feat: add router get test

--- router/router.go
+++ router/router.go
@@ -3,11 +3,11 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
+	// Initialize Router
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	// Initialize Router
+	initializeRoutes(r)
+
 	r.Run()
 }
